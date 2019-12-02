61d13d6005f2018849a49cc33517066a023677e3
Jordan <36084892+Jordan-Rowland@users.noreply.github.com>
2019-12-01 22:56:59 -0800

    day 2 tests

--- test2.py
+++ test2.py
@@ -1,9 +1,18 @@
-from day3 import *
+from day2 import *
 
 
 class TestDay2:
     def test1(self):
-        pass
+        assert opcodes([1, 0, 0, 0, 99]) == [2, 0, 0, 0, 99]
 
     def test2(self):
-        pass
+        assert opcodes([2, 3, 0, 3, 99]) == [2, 3, 0, 6, 99]
+
+    def test3(self):
+        assert opcodes([2, 4, 4, 5, 99, 0]) == [2, 4, 4, 5, 99, 9801]
+
+    def test4(self):
+        assert opcodes([1, 1, 1, 4, 99, 5, 6, 0, 99]) == [30, 1, 1, 4, 2, 5, 6, 0, 99]
+
+    def test_noun_verb(self):
+        assert noun_verb(19690720) == # Answer hidden
